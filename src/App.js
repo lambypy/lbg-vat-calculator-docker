e7264113a8a83a56406638898b635326ee1292de
lambypy <danbud@hotmail.co.uk>
2024-11-07 13:39:54 +0000

    Changed the div for my name header

--- src/App.js
+++ src/App.js
@@ -36,12 +36,14 @@ function App() {
   return (
     <div className='header field'>
       Daniel is here!!!
-      VAT CALCULATOR
-      <div className='colour-border'>
-        <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
-        <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
-        <DisplayBlock customstyle="field" label="VAT to pay: " value={vatToPay} />
-        <PriceEntryField customstyle="field" label="Price incl VAT: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0 ? "" : grossPrice} />
+      <div className='header field'>
+        VAT CALCULATOR
+        <div className='colour-border'>
+          <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
+          <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
+          <DisplayBlock customstyle="field" label="VAT to pay: " value={vatToPay} />
+          <PriceEntryField customstyle="field" label="Price incl VAT: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0 ? "" : grossPrice} />
+        </div>
       </div>
     </div>
   );
